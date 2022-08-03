a3bd2583e80e35a903e7479257ce4ec58312b566
audreythando <audreythando@gmail.com>
2022-08-03 21:37:05 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import React, {useState} from 'react';
+
+import Info from './components/Form/Info/Info';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeUsernameHandler = (e) => {
+      console.log("username change: ", e.target.value);
+      setUsername(e.target.value)
+  }
+  const changeEmailHandler = (e) => {
+      console.log("email change: ", e.target.value);
+      setEmail(e.target.value)
+  }
+  const changePasswordHandler = (e) => {
+      console.log("password change: ", e.target.value);
+      setPassword(e.target.value)
+
+  }
+
+  
+  const submit = (e) => {
+      e.preventDefault();
+  }
+
+  return (
+    <div className="App">
+      <div className='form'>
+            <h1>Login Form</h1>
+                <form className='login-form' onSubmit={submit}>
+                <div className='input-box'> 
+                    <input type="text" name="username" value={username} onChange={(e) => changeUsernameHandler(e)} placeholder='Enter Username' />
+                    </div>
+                    <div className='input-box'> 
+                    <input type="text" name="email" value={email} onChange={(e) => changeEmailHandler(e)} placeholder='Email' />
+                    </div>
+                     <div>
+                    <input type="password" name="password" value={password} onChange={(e) => changePasswordHandler(e)} placeholder='Password' />
+                    </div>
+                    {/* <div className='login-btn'>
+                        <button type='submit' onClick={submit}>LOGIN</button>
+                    </div> */}
+                </form>
+                {/* {<p>{username}</p>}
+                {<p>{email}</p>}
+                {<p>{password}</p>} */}
+                <Info name={username} email={email} password={password}/>
+            
+
+            </div>
+  
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Form/Info/Info.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure
+  } from '@chakra-ui/react'
+
+
+function Info(props) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <div>
+    <Button onClick={onOpen}>Login</Button>
+
+
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>User Details</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+           <p>{props.name}</p>
+           <p>{props.email}</p>
+           <p>{props.password}</p>
+
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme='blue' mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant='ghost'>Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+      
+    
+      
+    </div>
+  )
+    }
+
+export default Info
+
