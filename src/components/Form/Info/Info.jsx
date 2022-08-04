7576e2541b86654c64b7921a63607c860e8259e8
audreythando <audreythando@gmail.com>
2022-08-04 16:54:03 +0200

    Secondary button removed

--- src/components/Form/Info/Info.jsx
+++ src/components/Form/Info/Info.jsx
@@ -36,7 +36,6 @@ function Info(props) {
           <Button colorScheme='blue' mr={3} onClick={onClose}>
             Close
           </Button>
-          <Button variant='ghost'>Secondary Action</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
