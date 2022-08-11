812d7c4a78e21a2fec7e93946d74eeda5f1d83f0
audreythando <audreythando@gmail.com>
2022-08-11 13:37:56 +0200

    added image and styling to modal

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -8,7 +8,10 @@ import {
     ModalBody,
     ModalCloseButton,
     Button,
-    useDisclosure
+    useDisclosure,
+    Input,
+    Image,
+    Box
   } from '@chakra-ui/react'
 
 
@@ -21,22 +24,39 @@ function Details(props: ModalDetails) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div>
-    <Button onClick={onOpen}>Login</Button>
+    <Button onClick={onOpen} background={'#DD6B20'} mt={'15px'} position={'absolute'} width={'434px'}
+            borderRadius={'15px'} height={'53px'}>Login</Button>
 
 
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>User Details</ModalHeader>
+      <ModalContent background={'#F7F8FF'} width={'520px'}  height={'603px'} borderRadius={'90px'}>
+
+        <Box>
+      <Image src='./Images/logo.png' alt='logo' mb={'187px'} alignItems={'center'} width={'204px'} height={'66px'} position= {'absolute'} />
+      </Box>
+
+      <div>
+        <ModalHeader mt={'50px'} alignContent={'center'}>User Details</ModalHeader>
+        </div>
+
         <ModalCloseButton />
         <ModalBody>
-           <p>{props.email}</p>
-           <p>{props.password}</p>
 
+        
+      
+        <Input value={props.email}  variant='flushed' placeholder='Email' alignItems={'center'} fontFamily={'Roboto'} fontWeight={'100'} size={'18px'} 
+      margin={'35px'}  width={'-moz-fit-content'}/>
+        <Input value={props.password}  variant='flushed' placeholder='Password' alignItems={'center'} fontFamily={'Roboto'} fontWeight={'100'} size={'18px'}
+        margin={'35px'} width={'-moz-fit-content'} />
+
+           {/* <p>{props.email}</p>
+           <p>{props.password}</p> */}
+           
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme='blue' mr={3} onClick={onClose}>
+          <Button background={'#DD6B20'} borderRadius={'15px'} width={'350px'} height={'53px'} mb={'100px'} position={'absolute'} onClick={onClose}>
             Close
           </Button>
         </ModalFooter>
