a4307b0a67564c19cedabc8cbed51842588e0c3f
audreythando <audreythando@gmail.com>
2022-08-11 19:19:20 +0200

    added grid to style component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,63 +1,119 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import {
-    FormControl,
-    Heading,
-    Avatar,
-    Stack,
-    Input,
-    Flex,
-    Box,
-    Image
-  } from '@chakra-ui/react'
+  FormControl,
+  Heading,
+  Avatar,
+  Stack,
+  Input,
+  Flex,
+  Box,
+  Image,
+  Square,
+  SimpleGrid,
+  GridItem,
+  Grid
+} from '@chakra-ui/react'
 import Details from '../Details/Details';
 
-  
 function Form() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  
-  const changeEmailHandler = (e:React.ChangeEvent<any>): void => {
-      setEmail(e.target.value)
+
+  const changeEmailHandler = (e: React.ChangeEvent<any>): void => {
+    setEmail(e.target.value)
   }
-  const changePasswordHandler = (e:React.ChangeEvent<any>): void => {
-      setPassword(e.target.value)
+  const changePasswordHandler = (e: React.ChangeEvent<any>): void => {
+    setPassword(e.target.value)
 
   }
 
-  
-  const submit = (e:React.ChangeEvent<any>): void => {
-      e.preventDefault();
+
+  const submit = (e: React.ChangeEvent<any>): void => {
+    e.preventDefault();
 
   }
   return (
-    <Flex align="center" justify="center">
-    <div>
-    
-    
-    <Box width={'50%'}>
-  <Image src='./Images/picture.png' alt= 'picture' width={'727px'} height={'817px'} position={'absolute'} right={'700px'} mb={'150px'}/>  
+   
+<SimpleGrid columns={2} spacing={1}>
+
+ <Box  >
+  <Image src='./Images/picture.png' alt='picture 'height='1000px' /> 
   </Box>
 
-        <Stack spacing={6}>
-        <Heading  width={'132px'} height={'75px'} top={'925px'} alignContent={'center'}
-        left={'159px'} fontFamily={'Roboto'} fontWeight={'700'}
-        size={'30px'} lineHeight={'30px'} alignItems={'center'} letterSpacing={'15%'}>
-    Login Form
-  </Heading>
+  <Box  bg='#F7F8FF' height='100%'>
+  <Grid   h='700px' 
+  templateRows='repeat(3, 1fr)'
+  templateColumns='repeat(5, 1fr)'
+  gap={1}
+>
+  <GridItem colSpan={6} >
+  
+        <Heading 
+         fontFamily={'Roboto'} fontWeight={'700'}
+        size={'30px'}  letterSpacing={'15%'}>
+      Login Form
+       </Heading>
+
+       <Image src='./Images/logo.png' alt='logo' mb={'250px'} width={'313px'} height={'158px'} position={'absolute'} />
+
+       </GridItem> 
+
+  <GridItem colSpan={6} bg='blue' />
+  <GridItem colSpan={6} bg='blue' />
+  
+  
+  </Grid>
+  </Box>
+
+</SimpleGrid>
 
 
-  <Image src='./Images/logo.png' alt='logo'mb={'250px'} width={'313px'} height={'158px'} position= {'absolute'} />
-        </Stack>
-         
-    <FormControl onSubmit={submit} >
-     <Input variant='flushed' onChange={(e:any) => changeEmailHandler(e)} placeholder='email' value={email} />
-   <Input variant='flushed' onChange={(e:any) => changePasswordHandler(e)} placeholder='Password' value={password} />
-    
-     </FormControl>
-     <Details email={email} password={password} />
-      
-    </div></Flex>
+
+
+    // <Flex justify="space-between">
+
+    //   <Square>
+    //     1
+    //   </Square>
+
+    //   <Square>
+    //     <Flex direction="column" align="center">
+    //       <Box>222222222</Box>
+    //       <Box>3</Box>
+    //       <Box>4</Box>
+    //     </Flex>
+    //   </Square>
+
+    // </Flex>
+    // <Flex align="center" justify="center">
+
+    //   <Square maxWidth={'50%'}>
+    //     <Image src='./Images/picture.png' alt='picture ' />
+    //   </Square>
+
+
+    //   <Square maxWidth={'50%'} background={'#F7F8FF'}>
+    //     <Stack spacing={6}>
+    //       <Heading width={'132px'} height={'75px'} top={'925px'} alignContent={'center'}
+    //         left={'159px'} fontFamily={'Roboto'} fontWeight={'700'}
+    //         size={'30px'} lineHeight={'30px'} alignItems={'center'} letterSpacing={'15%'}>
+    //         Login Form
+    //       </Heading>
+
+
+    //       <Image src='./Images/logo.png' alt='logo' mb={'250px'} width={'313px'} height={'158px'} position={'absolute'} />
+    //     </Stack>
+
+    //     <FormControl onSubmit={submit} >
+    //       <Input variant='flushed' onChange={(e: any) => changeEmailHandler(e)} placeholder='email' value={email} />
+    //       <Input variant='flushed' onChange={(e: any) => changePasswordHandler(e)} placeholder='Password' value={password} />
+
+    //     </FormControl>
+    //     <Details email={email} password={password} />
+    //   </Square>
+
+    // </Flex>
   )
 }
 
