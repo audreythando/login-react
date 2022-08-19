08963ac7a231856e59f53922288746031c29bb9d
audreythando <audreythando@gmail.com>
2022-08-19 15:00:08 +0200

    refactor : fixed login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,7 +12,7 @@ import {
   useMediaQuery
 } from '@chakra-ui/react'
 import Details from '../Details/Details';
-import { ViewOffIcon } from '@chakra-ui/icons'
+import { ViewOffIcon, TriangleDownIcon } from '@chakra-ui/icons'
 
 
 function Form() {
@@ -22,7 +22,7 @@ function Form() {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [isMobile] = useMediaQuery('(min-width: 600px)')
+  const [isMobile] = useMediaQuery('(max-width: 600px)')
 
   const changeFullNameHandler = (e: React.ChangeEvent<any>): void => {
     setFullName(e.target.value)
@@ -38,63 +38,61 @@ function Form() {
 
   }
   return (
+    <Flex direction={isMobile ? 'column' : 'row'}  justify='center'>
 
-    <Flex   direction={isMobile ? 'row':'column'}  alignItems='center'>
-
-      <Center width= {isMobile ? '100%' : '50%'} >
-        <Image src='./Images/picture.png' />
-      </Center>
+    <Center width={isMobile ? '100%' : '50%'}>
+      <Image src='./Images/picture.png' />
+    </Center>
 
 
-      <Box width= {isMobile ? '100%' : '50%'} background='#F7F8FF'>
-      <Center flexDirection='column'  height='400px' background='#F7F8FF'>
+    <Box width= {isMobile ? '100%' : '50%'} background='#F7F8FF'>
+   <Center flexDirection='column' height='-webkit-fit-content'>
         <Box>
           <Heading
             fontFamily='Roboto' fontWeight='700'
-            size='30px' width='132px' height='75px' letterSpacing='15%' lineHeight='30px' mt='50px' >
-
+            size='30px' mt='30px' mb='15px' >
             Login
           </Heading>
         </Box>
 
         <Box>
-          <Image src='./Images/logo.png' alt='logo' width='313px' height='158px' mt='15px' />
+          <Image src='./Images/logo.png' alt='logo' width='313px' height='158px' />
         </Box>
 
+        <Box>
+          <InputGroup>
+            <InputRightElement
+              pointerEvents='none'
+            />
+            <Input variant='flushed' placeholder='Fullname' onChange={(e: React.ChangeEvent<any>): void => changeFullNameHandler(e)}
+              value={fullName} width='250px' />
+          </InputGroup>
+
+        </Box>
 
         <Box>
-<InputGroup>
-    <InputRightElement
-      pointerEvents='none'
-    />
-    <Input variant='flushed'  placeholder='Fullname' onChange={(e: React.ChangeEvent<any>): void => changeFullNameHandler(e)}  
-     value={fullName}/>
-  </InputGroup>
+          <InputGroup>
+            <InputRightElement
+              pointerEvents='none'
+            />
+            <Input variant='flushed' placeholder='Email' onChange={(e: React.ChangeEvent<any>): void => changeEmailHandler(e)}
+              value={email} width='250px' />
+          </InputGroup>
 
-</Box>
+        </Box>
 
-<Box>
-<InputGroup>
-    <InputRightElement
-      pointerEvents='none'
-    />
-    <Input variant='flushed'  placeholder='Email' onChange={(e: React.ChangeEvent<any>): void => changeEmailHandler(e)}  
-     value={email}/>
-  </InputGroup>
+        <Box>
+          <InputGroup>
+            <InputRightElement
+              pointerEvents='none'
+              children={<ViewOffIcon color='gray.300' />}
+            />
+            <Input variant='flushed' type='password' placeholder='Password' onChange={(e: React.ChangeEvent<any>): void => changePasswordHandler(e)}
+              value={password} width='250px' />
+          </InputGroup>
 
-</Box>
+        </Box>
 
-<Box>
-<InputGroup>
-    <InputRightElement
-      pointerEvents='none'
-      children={<ViewOffIcon color='gray.300' />}
-    />
-    <Input variant='flushed' type='password' placeholder='Password' onChange={(e: React.ChangeEvent<any>): void => changePasswordHandler(e)}  
-     value={password}/>
-  </InputGroup>
-
-</Box>
 
 
         <Box display='flex' alignItems='center'>
@@ -103,18 +101,18 @@ function Form() {
 
         </Box>
 
-        <h2 text-align='center' color=' #000000' font-family='Roboto' font-style='normal' font-size='13px' font-weight='100' line-height='40px'>Do not have an account ? SignUp</h2>
+        <h4 text-align='center' color=' #000000' font-family='Roboto' font-style='normal' font-size='13px' font-weight='100' line-height='40px'>Do not have an account ? SignUp</h4>
 
 
       </Center>
-      </Box>
+    </Box>
 
-    </Flex>
-
-
+  </Flex>
 
 
-  )
+
+)
 }
+
 
 export default Form
